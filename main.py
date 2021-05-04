f1704f60c971726b8b96c29789dd8f65b2578562
kingatan7 <kinga-tanska@wp.pl>
2021-05-04 18:56:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,117 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import cv2
+import argparse
+from PIL import Image
+import os
+
+def load_images_to_data(image_label, image_directory, features_data, label_data):
+    list_of_files = os.listdir(image_directory)
+    for file in list_of_files:
+        image_file_name = os.path.join(image_directory, file)
+        if ".jpg" in image_file_name:
+            print(image_file_name)
+            img = Image.open(image_file_name).convert("L")
+            img = np.resize(img, (28,28,1))
+            im2arr = np.array(img)
+            im2arr = im2arr.reshape(1,28,28,1)
+            features_data = np.append(features_data, im2arr, axis=0)
+            label_data = np.append(label_data, [image_label], axis=0)
+    return features_data, label_data
+
+def train():
+    (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+    num_classes = 10
+    input_shape = (28, 28, 1)
+
+    # x_train = x_train.astype("float32") / 255
+    # x_test = x_test.astype("float32") / 255
+    # x_train = np.expand_dims(x_train, -1)
+    # x_test = np.expand_dims(x_test, -1)
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1).astype('float32')
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1).astype('float32')
+    print("x_train shape:", X_train.shape)
+    print(X_train.shape[0], "train samples")
+    print(X_test.shape[0], "test samples")
+    X_test, y_test = load_images_to_data('1', 'skany\\skany\\Kinia', X_test, y_test)
+    # normalize inputs from 0-255 to 0-1
+    X_train /= 255
+    X_test /= 255
+
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    print("Data type for input: ", X_train.dtype)
+    print("Tensor shape for input: ", X_train.shape)
+
+    model = keras.Sequential(
+        [
+            keras.Input(shape=input_shape),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
+
+    model.summary()
+
+    batch_size = 128
+    epochs = 1
+
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+    model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+    score = model.evaluate(X_test, y_test, verbose=0)
+    test_examples = y_test.shape[0]
+
+    y = model.predict(X_test[:test_examples])
+    print("Test loss:", score[0])
+    print("Test accuracy:", score[1])
+
+    detections = tf.math.argmax(y[:test_examples], 1)
+    true_values = tf.math.argmax(y_test[:test_examples], 1)
+    cm = tf.math.confusion_matrix(true_values, detections)
+    print("Confusion Matrix: ")
+    print(cm.numpy())
+
+    # Define functions to count TP, FP, FN, recall and precision for each class
+
+    def false_positives(conf_matrix):
+        sums = np.sum(conf_matrix, axis=0)
+        fp = np.subtract(sums, np.diagonal(conf_matrix))
+        return fp
+
+    def false_negatives(conf_matrix):
+        sums = np.sum(conf_matrix, axis=1)
+        fn = np.subtract(sums, np.diagonal(conf_matrix))
+        return fn
+
+    def recall(conf_matrix):
+        tp = np.diagonal(conf_matrix)
+        fn = false_negatives(conf_matrix)
+        return (tp / (tp + fn))
+
+    def precision(conf_matrix):
+        tp = np.diagonal(conf_matrix)
+        fp = false_positives(conf_matrix)
+        return (tp / (tp + fp))
+
+    print("Recall: ", 100 * recall(cm))
+    print("Precision: ", 100 * precision(cm))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Training')
+    parser.add_argument('--train', '-t', default='False', help='train mode')
+    args = parser.parse_args()
+    print(args.train)
+    #if args.train == "True":
+    train()
